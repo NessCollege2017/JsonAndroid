6ccd0f01bbc3e1f5081e82cd50b78f882140e1a2
NessCollege <nesscollege2017@gmail.com>
2017-05-16 20:53:18 +0300

    Threads  run on ui thread

--- app/src/main/java/ness/edu/jsonandroid/MainActivity.java
+++ app/src/main/java/ness/edu/jsonandroid/MainActivity.java
@@ -14,22 +14,70 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Movie> movies = new ArrayList<>();
+    TextView tvMovies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        tvMovies = (TextView) findViewById(R.id.tvMovies);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MyThread myThread = new MyThread();
-                myThread.start();
+                Thread movieThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //code that runs in the background
+                        //Uses-permission INTERNET in manifest
+                        try {
+                            String json = StreamIO.readWebSite("http://api.androidhive.info/json/movies.json");
+                            //Decide if jsonObject Or JSON Array;
+                            JSONArray moviesArray = new JSONArray(json);
+                            for (int i = 0; i < moviesArray.length(); i++) {
+                                ArrayList<String> genres = new ArrayList<>();
+                                JSONObject movieObject = moviesArray.getJSONObject(i);
+                                String title = movieObject.getString("title");
+                                String image = movieObject.getString("image");
+                                int releaseYear = movieObject.getInt("releaseYear");
+                                double rating = movieObject.getDouble("rating");
+                                JSONArray genresArray = movieObject.getJSONArray("genre");
+                                for (int j = 0; j < genresArray.length(); j++) {
+                                    String genre = genresArray.getString(j);
+                                    genres.add(genre);
+                                }
+                                Movie movie = new Movie(title, image, genres, releaseYear, rating);
+                                movies.add(movie);
+                            }
+                            Log.d("Ness", movies.toString());
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //code that runs on the UI Thread:
+                                    tvMovies.setText(movies.toString());
+                                }
+                            });
+                        } catch (IOException e) {
+                            //TODO: Handle with dialog
+                            e.printStackTrace();
+
+                        } catch (JSONException ignored) {}
+                    }
+                });
+                movieThread.start();
             }
         });
     }
--- app/src/main/java/ness/edu/jsonandroid/MyThread.java
+++ app/src/main/java/ness/edu/jsonandroid/MyThread.java
@@ -9,38 +9,11 @@ import org.json.JSONObject;
 import java.io.Console;
 import java.io.IOException;
 
-/**
- * Created by Android2017 on 5/16/2017.
- */
+
 
 public class MyThread extends Thread {
     @Override
     public void run() {
-        //Uses-permission INTERNET in manifest
-        try {
-            String json = StreamIO.readWebSite("http://api.androidhive.info/json/movies.json");
-            //Decide if jsonObject Or JSON Array;
-            JSONArray moviesArray = new JSONArray(json);
-            for (int i = 0; i < moviesArray.length(); i++) {
-                JSONObject movieObject = moviesArray.getJSONObject(i);
-                String title = movieObject.getString("title");
 
-                String image = movieObject.getString("image");
-
-                int releaseYear = movieObject.getInt("releaseYear");
-
-                double rating = movieObject.getDouble("rating");
-
-                JSONArray genresArray = movieObject.getJSONArray("genre");
-
-                for (int j = 0; j < genresArray.length(); j++) {
-                    String genre = genresArray.getString(j);
-                }
-            }
-        } catch (IOException e) {
-            //TODO: Handle with dialog
-            e.printStackTrace();
-
-        } catch (JSONException ignored) {}
     }
 }
