c69a094a71ca4292d81b259095736ca2d002d0b9
NessCollege <nesscollege2017@gmail.com>
2017-05-16 18:59:20 +0300

    Initial commit

---
+++ app/src/main/java/ness/edu/jsonandroid/MyThread.java
@@ -0,0 +1,14 @@
+package ness.edu.jsonandroid;
+
+import android.util.Log;
+
+/**
+ * Created by Android2017 on 5/16/2017.
+ */
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+
+    }
+}
